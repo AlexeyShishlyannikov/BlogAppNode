6de623cd39d2b50ed9189c9dd1042653a0646477
unknown <shvonder95@gmail.com>
2017-11-11 15:28:35 -0800

    Started UI

--- routes/auth.js
+++ routes/auth.js
@@ -15,6 +15,18 @@ router.get('/google/callback',
 		res.redirect('/dashboard');
 	});
 
+router.get('/verify', (req, res) => {
+	if(req.user){
+
+	} else {
+
+	}
+});
+
+router.get('/logout', (req, res) => {
+	req.logout();
+	res.redirect('/');
+});
 
 
 
