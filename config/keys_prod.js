3d9a0ae37142679207a43894d18fb0f524516327
unknown <shvonder95@gmail.com>
2017-11-13 15:59:01 -0800

    Fixed env vars

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,5 +1,5 @@
 module.exports = {
 	mongoURI: process.env.MONGO_URI,
-	googleClientID: process.enc.GOOGLE_CLIENT_ID,
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
 	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET
 }
